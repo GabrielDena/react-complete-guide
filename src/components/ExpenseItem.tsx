8b9198d47924e1d506cbbf7793637e56acbdbe79
Gabriel Dena <gabrieldenac@hotmail.com>
2023-02-10 18:38:21 -0300

    + Props expense + Editorconfig + Prettier

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,14 +1,10 @@
-export default function ExpenseItem() {
-    const expenseDate = new Date();
-    const expenseTitle = 'Airplane Tickets';
-    const expenseAmount = 95000;
-
+export default function ExpenseItem({ date, title, amount }: { date: Date; title: string; amount: number }) {
     return (
         <div className='flex w-4/6 items-center justify-start rounded-xl bg-zinc-600 p-2 align-middle text-base shadow-lg'>
-            <div className='w-1/6'>{expenseDate.toISOString().split('T')[0]}</div>
+            <div className='w-1/6'>{date.toISOString().split('T')[0]}</div>
             <div className='flex w-full items-center justify-between align-middle font-bold text-white'>
-                <h2>{expenseTitle}</h2>
-                <div className='rounded-xl border bg-purple-900 px-4 py-2'>R${expenseAmount}</div>
+                <h2>{title}</h2>
+                <div className='rounded-xl border bg-purple-900 px-4 py-2'>R${amount}</div>
             </div>
         </div>
     );
